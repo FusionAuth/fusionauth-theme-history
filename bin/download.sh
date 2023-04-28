bc94464fd673067c1e3b8be58472697d447d5db3
Dan Moore <dan@fusionauth.io>
2023-04-28 10:02:25 -0600

    default themes download changes
    don't need localized messages
    remove all extra json
    make curl silent

--- bin/download.sh
+++ bin/download.sh
@@ -3,7 +3,7 @@
 
 mkdir -p $TMP_DIR
 
-curl -H 'Authorization: '$API_KEY  $FUSIONAUTH_URL/api/theme/$THEME_ID > $TMP_DIR/themeout.json
+curl -s -H 'Authorization: '$API_KEY  $FUSIONAUTH_URL/api/theme/$THEME_ID > $TMP_DIR/themeout.json
 cat $TMP_DIR/themeout.json |jq '.theme.templates' > $TMP_DIR/templates.json
 
 # splits template files
@@ -11,13 +11,8 @@ node splitfiles.js templates.json .ftl
 
 cat $TMP_DIR/themeout.json |jq '.theme.defaultMessages'  |sed 's/^"//' |sed 's/"$//' |node convert-newlines.js > $TMP_DIR/defaultMessages.txt
 
-cat $TMP_DIR/themeout.json |jq '.theme.localizedMessages' > $TMP_DIR/localizedMessages.json
-node splitfiles.js localizedMessages.json .txt
-
 # empty string if null https://stackoverflow.com/questions/53135035/jq-returning-null-as-string-if-the-json-is-empty
 cat $TMP_DIR/themeout.json |jq '.theme.stylesheet // empty'  |sed 's/^"//' |sed 's/"$//' |node convert-newlines.js > $TMP_DIR/styles.css
 
-#curl -H 'Authorization: '$API_KEY  $FUSIONAUTH_URL/api/theme/$THEME_ID|jq '.theme.localizedMessages' > $TMP_DIR/localizedMessages.properties
 
-
-rm $TMP_DIR/templates.json $TMP_DIR/themeout.json
+rm $TMP_DIR/*.json
